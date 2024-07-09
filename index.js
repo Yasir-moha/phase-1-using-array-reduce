8a30bc5b870b1dd5480578d173ef7cc1ba8cb785
Yasir Mohamed <bigslater30@gmail.com>
2024-07-09 10:12:46 +0300

    my file

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Define the batteryBatches array
+const batteryBatches = [
+    { batteryAmount: 10 },
+    { batteryAmount: 10 },
+    { batteryAmount: 11 }
+  ];
+  
+  // Use reduce to aggregate the total number of batteries
+  const totalBatteries = batteryBatches.reduce((total, batch) => {
+    return total + batch.batteryAmount;
+  }, 0);
+  
+  // Log the total number of batteries to the console
+  console.log(totalBatteries); // Output: 31
+  
+  // Exporting the totalBatteries variable (if needed in a module system)
+  module.exports = totalBatteries;
+  
